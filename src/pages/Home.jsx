9833a30c8604dae35f5e180a6166d821f7febaf4
Khalil <khalilundo12@gmail.com>
2025-03-07 00:59:44 +0800

    deployment issue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
-import logo from '../assets/heroimage.svg'
 import Navbar from '../components/Navbar/navbar';
-import Hero from '../components/hero/hero';
+import Hero from '../components/Hero/hero';
 
 export default function Home() {
     return (
